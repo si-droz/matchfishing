f0cb33166f6f95516d967f4715b9dd33fa7ca517
Simon Drozdziol <simon.drozdziol@outlook.com>
2017-03-30 20:02:47 +0100

    Added pairs calculation

--- Code/Match.Fishing.Web/app/src/areas/public/components/matches/match-details.component.js
+++ Code/Match.Fishing.Web/app/src/areas/public/components/matches/match-details.component.js
@@ -21,10 +21,53 @@
     var controller = function ($http) {
         var model = this;
         model.match = null;
+        model.pairs = [];
 
         model.$routerOnActivate = function (next) {
             getMatch($http, next.params.id).then(function (match) {
                 model.match = match;
+
+                if (model.match.isPairs) {
+                    var pairs = []
+                    for (var index = 0; index < match.matchEntries.length; index++) {
+                        var matchEntry = match.matchEntries[index];
+
+                        var toAdd = true;
+                        pairs.forEach(function (pair) {
+                            if (pair.peg1 == matchEntry.peg || pair.peg2 == matchEntry.peg) {
+                                toAdd = false;
+                            }
+                        }, this);
+
+                        if (toAdd) {
+                            var matchEntryPair = [];
+
+                            matchEntryPair.push(matchEntry);
+
+                            match.matchEntries.forEach(function (me) {
+                                if (matchEntry.pairedWithPeg == me.peg) {
+                                    matchEntryPair.push(me);
+                                }
+                            }, this);
+
+                            var pair = {
+                                peg1: matchEntryPair[0].peg,
+                                peg2: matchEntryPair[1].peg,
+                                angler1: matchEntryPair[0].anglerName,
+                                angler2: matchEntryPair[1].anglerName,
+                                weight: matchEntryPair[0].weight + matchEntryPair[1].weight
+                            }
+
+                            if (pair.peg1 == pair.peg2) {
+                                pair.peg2 = null
+                                pair.angler2 = '(weight doubled)'
+                            }
+
+                            pairs.push(pair);
+                        }
+                    }
+                    model.pairs = pairs;
+                }
             });
         };
     };
