6e27db194c789a91db5d4e38a16df3ea14a039ce
Simon Drozdziol <simon.drozdziol@m-kopa.com>
2019-03-09 22:50:27 +0000

    Removed dependency on ui-bootstrap

--- Code/Match.Fishing.Web/app/app.js
+++ Code/Match.Fishing.Web/app/app.js
@@ -4,8 +4,7 @@
   var module = angular.module("matchFishing", [
     "ngComponentRouter"
     , "ngAnimate"
-    , "ngMessages"
-    , "ui.bootstrap"
+    , "ngMessages"    
   ]).constant("EnvironmentConfig",
   {
       "serviceApi": "http://localhost:61573"
